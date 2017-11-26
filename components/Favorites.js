cf06e696adc76625090150b4888fc9337e239ba9
Thiago Brezinski <thiagobrez@gmail.com>
2017-11-26 00:53:32 -0200

    Added thumnails, scrollView and remove button to Favorites component.

--- components/Favorites.js
+++ components/Favorites.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
 import {
 	List,
 	ListItem,
 	Text,
+	Thumbnail,
+	Body,
+	Button,
+	Icon
 } from 'native-base';
 
 export default class Favorites extends Component<{}> {
 	
 	render() {
 		return (
-			<List>
-				{this.props.locations.map(location => (
-					<ListItem key={location.id}>
-						<Text>
-							{location.street}
-						</Text>
-					</ListItem>
-				))}
-			</List>
+			<ScrollView>
+				<List>
+					{this.props.locations.map(location => (
+						<ListItem key={location.id}>
+							<Thumbnail square size={80} source={{ uri: 'https://i.ytimg.com/vi/Xx6t0gmQ_Tw/maxresdefault.jpg' }} />
+							<Body>
+								<Text>{location.street}</Text>
+								<Text note>{location.neighborhood}</Text>
+							</Body>
+							<Button transparent danger>
+								<Text>Remove</Text>
+							</Button>
+						</ListItem>
+					))}
+				</List>
+			</ScrollView>
 		)
 	}
 	
-}
+};
